078d7e35337647ee71414e3fc5b5ef3a4d289094
Jeff Schwane <jschwane@gmail.com>
2020-12-21 17:43:46 -0500

    Completed 13 Labs

--- 13_list_comprehensions/13_04_fish.py
+++ 13_list_comprehensions/13_04_fish.py
@@ -7,3 +7,6 @@ Tip: Use an if statement in the listcomp
 '''
 
 fish_tuple = ('blowfish', 'clownfish', 'catfish', 'octopus')
+
+fish_endings = [fish for fish in fish_tuple if fish.endswith('fish')]
+print(fish_endings)
--- 13_list_comprehensions/13_05_shirt_shop.py
+++ 13_list_comprehensions/13_05_shirt_shop.py
@@ -8,3 +8,5 @@ Then open up your online shop ;)
 
 colors = ["neon orange", "spring green"]
 sizes = ["S", "M", "L"]
+cart_prod = [(a, b) for a in colors for b in sizes]
+print(cart_prod)
