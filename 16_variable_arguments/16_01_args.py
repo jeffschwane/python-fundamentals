0a608fb1e909f0b9688c73e1cb2e67856d806e49
Jeff Schwane <jschwane@gmail.com>
2020-12-24 11:10:23 -0500

    Completed 16 Labs

--- 16_variable_arguments/16_01_args.py
+++ 16_variable_arguments/16_01_args.py
@@ -2,3 +2,10 @@
 Write a script with a function that demonstrates the use of *args.
 
 '''
+
+
+def my_funct(number, *args):
+    return number + sum(args)
+
+
+print(my_funct(5, 6, 7, 8))
--- 16_variable_arguments/16_02_kwargs.py
+++ 16_variable_arguments/16_02_kwargs.py
@@ -2,3 +2,11 @@
 Write a script with a function that demonstrates the use of **kwargs.
 
 '''
+
+
+def my_funct(**kwargs):
+    for k, v in kwargs.items():
+        print(f'{k}: {v}')
+
+
+my_funct(living_room='tripod light', kitchen='light strip')
