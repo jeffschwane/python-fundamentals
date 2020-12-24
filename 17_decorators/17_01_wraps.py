ddc505215bcca30c6fb307cd9945a7536b415ba8
Jeff Schwane <jschwane@gmail.com>
2020-12-24 13:57:58 -0500

    Completed 17 Labs

--- 17_decorators/17_01_wraps.py
+++ 17_decorators/17_01_wraps.py
@@ -2,3 +2,17 @@
 Write a decorator function that wraps text passed to it in a html <p> tag.
 
 '''
+
+
+def decorator_func(initial_func):
+    def wrapper_func():
+        return '<p> ' + initial_func() + ' </p>'
+    return wrapper_func
+
+
+@decorator_func
+def html_text():
+    return 'This is my html text'
+
+
+print(html_text())
--- 17_decorators/17_02_tags.py
+++ 17_decorators/17_02_tags.py
@@ -3,3 +3,19 @@ Improve the decorator from the previous exercise by allowing it to take
 any specified HTML tag as an input - making it more general.
 
 '''
+
+
+def decorator_arg(tag):
+    def decorator_func(initial_func):
+        def wrapper_func():
+            return f'<{tag}> {initial_func()} </{tag}>'
+        return wrapper_func
+    return decorator_func
+
+
+@decorator_arg('h')
+def html_text():
+    return 'This is my html text'
+
+
+print(html_text())
